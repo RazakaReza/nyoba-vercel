a711b4bb5fe7e0918966cc9cc799d011552f2d09
Razaka <razakareza31@gmail.com>
2025-12-02 20:13:42 +0700

    vendor a

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const app = express();
 const PORT = 3000;
 app.use(express.json());
 
-
+const dataVendorA = [
+  { "kd_produk": "A001", "nm_brg": "Kopi Bubuk 100g", "hrg": "15000", "ket_stok": "ada" },
+  { "kd_produk": "A002", "nm_brg": "Gula Merah", "hrg": "12500", "ket_stok": "habis" }
+];
 
 
 app.listen(PORT, () => {
