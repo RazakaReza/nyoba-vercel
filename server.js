30dd2735b6d308495cf222a43c165359b2725094
FadillPro <carleskar276@gmail.com>
2025-12-03 09:52:00 +0700

    nambah vendor c

--- server.js
+++ server.js
@@ -12,6 +12,21 @@ const dataVendorB = [
   { "sku": "HOODIE-005", "productName": "Hoodie Kawah Wurung", "price": 150000, "isAvailable":false}
 ];
 
+const dataVendorC = [
+  {
+    "id": 501,
+    "details": { "name": "Nasi Tempong", "category": "Food" },
+    "pricing": { "base_price": 20000, "tax": 2000 },
+    "stock": 50
+  },
+  {
+    "id": 502,
+    "details": { "name": "Es Jeruk", "category": "Drink" },
+    "pricing": { "base_price": 8000, "tax": 500 },
+    "stock": 0
+  }
+];
+
 
 app.listen(PORT, () => {
   console.log(`Server berjalan di localhost::${PORT}`);
