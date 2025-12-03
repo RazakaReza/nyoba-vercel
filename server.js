144381f9104e60bac8297a9705d0889714aaaee0
FadillPro <carleskar276@gmail.com>
2025-12-03 09:50:22 +0700

    update

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const dataVendorA = [
 ];
 const dataVendorB = [
   { "sku": "TSHIRT-001", "productName": "Kaos Ijen Crater", "price": 75000, "isAvailable": true },
-  { "sku": "HOODIE-005", "productName": "Hoodie Kawah Wurung", "price": 150000, "isAvailable": false }
+  { "sku": "HOODIE-005", "productName": "Hoodie Kawah Wurung", "price": 150000, "isAvailable":false}
 ];
 
 
