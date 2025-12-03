27186bfc46a7a8e08dfc196551db4d2fcfd308cb
FadillPro <carleskar276@gmail.com>
2025-12-03 10:09:59 +0700

    namba data vendor C

--- server.js
+++ server.js
@@ -4,19 +4,68 @@ const PORT = 3000;
 app.use(express.json());
 
 const dataVendorA = [
-  { "kd_produk": "A001", "nm_brg": "Kopi Bubuk 100g", "hrg": "15000", "ket_stok": "ada" },
-  { "kd_produk": "A002", "nm_brg": "Gula Merah", "hrg": "12500", "ket_stok": "habis" },
-  {"kd_produk": "A003", "nm_brg": "Gula Pasir 1kg", "hrg": "3000", "ket_stok": "ada"},
-  {"kd_produk": "A004", "nm_brg": "Tepung 1kg", "hrg": "5000", "ket_stok": "habis"},
-  {"kd_produk": "A005", "nm_brg": "Rokok Gajah Baru", "hrg": "18000", "ket_stok": "ada"}
+  { 
+    "kd_produk": "A001",
+    "nm_brg": "Kopi Bubuk 100g",
+    "hrg": "15000",
+    "ket_stok": "ada"
+  },
+  { 
+    "kd_produk": "A002",
+    "nm_brg": "Gula Merah",
+    "hrg": "12500",
+    "ket_stok": "habis"
+  },
+  {
+    "kd_produk": "A003",
+    "nm_brg": "Gula Pasir 1kg",
+    "hrg": "3000",
+    "ket_stok": "ada"
+  },
+  {
+    "kd_produk": "A004",
+    "nm_brg": "Tepung 1kg",
+    "hrg": "5000",
+    "ket_stok": "habis"
+  },
+  {
+    "kd_produk": "A005",
+    "nm_brg": "Rokok Gajah Baru",
+    "hrg": "18000",
+    "ket_stok": "ada"
+  }
 ];
 
 const dataVendorB = [
-  { "sku": "TSHIRT-001", "productName": "Kaos Ijen Crater", "price": 75000, "isAvailable": true },
-  { "sku": "HOODIE-005", "productName": "Hoodie Kawah Wurung", "price": 150000, "isAvailable":false},
-  { "sku": "CAP-002", "productName": "Topi Gunung Bromo", "price": 50000, "isAvailable": true },
-  { "sku": "JACKET-003", "productName": "Jaket Ranu Kumbolo", "price": 200000, "isAvailable": true },
-  { "sku": "BACKPACK-004", "productName": "Tas Pendakian Semeru", "price": 300000, "isAvailable": false }
+  { 
+    "sku": "TSHIRT-001", 
+    "productName": "Kaos Ijen Crater",
+    "price": 75000, 
+    "isAvailable": true 
+  },
+  { 
+    "sku": "HOODIE-005",
+    "productName": "Hoodie Kawah Wurung",
+    "price": 150000,
+    "isAvailable":false
+  },
+  { "sku": "CAP-002",
+    "productName": "Topi Gunung Bromo",
+    "price": 50000,
+    "isAvailable": true
+  },
+  { 
+    "sku": "JACKET-003",
+    "productName": "Jaket Ranu Kumbolo",
+    "price": 200000,
+    "isAvailable": true
+  },
+  { 
+    "sku": "BACKPACK-004",
+    "productName": "Tas Pendakian Semeru",
+    "price": 300000,
+    "isAvailable": false
+  }
 ];
 
 const dataVendorC = [
@@ -31,7 +80,25 @@ const dataVendorC = [
     "details": { "name": "Es Jeruk", "category": "Drink" },
     "pricing": { "base_price": 8000, "tax": 500 },
     "stock": 0
-  }
+  },
+  {
+    "id": 503,
+    "details": { "name": "Ayam Geprekx", "category": "Food" },
+    "pricing": { "base_price": 10000, "tax": 3000 },
+    "stock": 6
+  },
+  {
+    "id": 504,
+    "details": { "name": "Soto Betawi", "category": "Food" },
+    "pricing": { "base_price": 15000, "tax": 2000 },
+    "stock": 16
+  },
+  {
+    "id": 505,
+    "details": { "name": "Es Buah", "category": "Drink" },
+    "pricing": { "base_price": 5000, "tax": 1000 },
+    "stock": 0
+  },
 ];
 
 
