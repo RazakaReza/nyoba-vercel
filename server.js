276bdef3eb2916110d2407e82612585d60c9e550
farhanganteng@gmail.com <farhanganteng@gmail.com>
2025-12-03 10:00:15 +0700

    update

--- server.js
+++ server.js
@@ -13,7 +13,10 @@ const dataVendorA = [
 
 const dataVendorB = [
   { "sku": "TSHIRT-001", "productName": "Kaos Ijen Crater", "price": 75000, "isAvailable": true },
-  { "sku": "HOODIE-005", "productName": "Hoodie Kawah Wurung", "price": 150000, "isAvailable":false}
+  { "sku": "HOODIE-005", "productName": "Hoodie Kawah Wurung", "price": 150000, "isAvailable":false},
+  { "sku": "CAP-002", "productName": "Topi Gunung Bromo", "price": 50000, "isAvailable": true },
+  { "sku": "JACKET-003", "productName": "Jaket Ranu Kumbolo", "price": 200000, "isAvailable": true },
+  { "sku": "BACKPACK-004", "productName": "Tas Pendakian Semeru", "price": 300000, "isAvailable": false }
 ];
 
 const dataVendorC = [
