4accbaac5935d5f8946c66057933fb28881d5a8b
Razaka <razakareza31@gmail.com>
2025-12-03 13:47:18 +0700

    pembaruannnnn

--- api/data.js
+++
@@ -1,9 +0,0 @@
-import dataVendorA from "./vendor_a";
-import dataVendorB from "./vendor_b";
-import dataVendorC from "./vendor_c";
-
-export default function handler(req, res) {
-  const final = [...dataVendorA(req, res, true), ...dataVendorB(req, res, true), ...dataVendorC(req, res, true)];
-
-  res.status(200).json(final);
-}
