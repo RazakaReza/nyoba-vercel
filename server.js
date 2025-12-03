aa1716099dd7b2824466f1abe54e4f5a74ec00cd
farhanganteng@gmail.com <farhanganteng@gmail.com>
2025-12-03 09:47:51 +0700

    update

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require ('express');
 const app = express();
 const PORT = 3000;
 app.use(express.json());
@@ -6,6 +6,10 @@ app.use(express.json());
 const dataVendorA = [
   { "kd_produk": "A001", "nm_brg": "Kopi Bubuk 100g", "hrg": "15000", "ket_stok": "ada" },
   { "kd_produk": "A002", "nm_brg": "Gula Merah", "hrg": "12500", "ket_stok": "habis" }
+];
+const dataVendorB = [
+  { "sku": "TSHIRT-001", "productName": "Kaos Ijen Crater", "price": 75000, "isAvailable": true },
+  { "sku": "HOODIE-005", "productName": "Hoodie Kawah Wurung", "price": 150000, "isAvailable": false }
 ];
 
 
